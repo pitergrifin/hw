c57b428bdaa83824b6261515b17c0bd6fe0111ba
Daniil Izmodenov <d.izmodenov@insilicomedicine.com>
2025-09-09 17:47:54 +0400

    hw2

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -1,7 +1,8 @@
 #include <vector>
 #include <utility>
 #include <numeric>
-#include <iostream>
+#include <unordered_set>
+#include <queue>
 #include "hw2.hpp"
 
 int sumOfNodes(TreeNode* head) { 
@@ -36,4 +37,63 @@ int sumOfNodes(TreeNode* head) {
     }
     
     return sum; 
+}
+
+std::vector<int> Solution::intersection(std::vector<int>& nums1,
+                                        std::vector<int>& nums2) {
+    auto nums1Set = std::unordered_set<int>(nums1.begin(), nums1.end());
+    std::unordered_set<int> resSet;
+    for (int el : nums2)
+        if (nums1Set.count(el)) resSet.insert(el);
+    return std::vector<int>(resSet.begin(), resSet.end());
+}
+
+std::vector<int> Solution::distanceK(TreeNode* root, TreeNode* target, int k) {
+    std::unordered_map<int, std::pair<TreeNode*, TreeNode*>> nodeMap;
+    nodeMap[root->val] = {root, nullptr};
+    std::vector<TreeNode*> currChild{root};
+    while (not currChild.empty()) {
+        std::vector<TreeNode*> nextChild;
+        for(auto node : currChild) {
+            if (node->left) {
+                nextChild.push_back(node->left);
+                nodeMap[node->left->val] = {node->left, node};
+            }
+            if (node->right) {
+                nextChild.push_back(node->right);
+                nodeMap[node->right->val] = {node->right, node};
+            }
+        }
+        currChild = std::move(nextChild);
+    }
+    
+    std::queue<int> bfsQ;
+    std::unordered_set<int> visited;
+    bfsQ.push(target->val);
+    while (not bfsQ.empty())
+    {
+        if (k == 0) {
+            std::vector<int> res;
+            res.reserve(bfsQ.size());
+            while (not bfsQ.empty()) {
+                res.push_back(bfsQ.front());
+                bfsQ.pop();
+            }
+            return res;
+        }
+        size_t N = bfsQ.size();
+        for (int i{ 0 }; i < N; ++i) {
+            auto node = nodeMap[bfsQ.front()];
+            visited.insert(node.first->val);
+            bfsQ.pop();
+            if (node.second and not visited.count(node.second->val))
+                bfsQ.push(node.second->val);
+            if (node.first->left and not visited.count(node.first->left->val))
+                bfsQ.push(node.first->left->val);
+            if (node.first->right and not visited.count(node.first->right->val))
+                bfsQ.push(node.first->right->val);
+        }
+        --k;
+    }
+    return std::vector<int>();
 }
--- hw2/hw2.hpp
+++ hw2/hw2.hpp
@@ -1,3 +1,8 @@
 #include "Tree.hpp"
 
 int sumOfNodes(TreeNode* head);
+class Solution {
+public:
+    std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2); 
+    std::vector<int> distanceK(TreeNode* root, TreeNode* target, int k);
+};
